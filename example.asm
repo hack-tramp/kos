bfe5af91bd73e4b93af6872ca5cadf24d6941c57
hack-tramp <53376845+hack-tramp@users.noreply.github.com>
2026-01-07 20:50:58 +0800

    Change CA root certificate and SSL config
    Updated the CA root certificate to ISRG Root X1 and modified the SSL/TLS configuration for required verification.

--- example.asm
+++ example.asm
@@ -35,6 +35,46 @@ include '../../../../../struct.inc'
 include 'mbedtls.inc'
 
 
+;------------------------------------------------------------
+; Real CA: ISRG Root X1 (Let’s Encrypt)
+;------------------------------------------------------------
+
+isrg_root_x1_pem db "-----BEGIN CERTIFICATE-----",13,10,\
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw",13,10,\
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh",13,10,\
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4",13,10,\
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu",13,10,\
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY",13,10,\
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc",13,10,\
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+",13,10,\
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U",13,10,\
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW",13,10,\
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH",13,10,\
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC",13,10,\
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv",13,10,\
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn",13,10,\
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn",13,10,\
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw",13,10,\
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI",13,10,\
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV",13,10,\
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq",13,10,\
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL",13,10,\
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ",13,10,\
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK",13,10,\
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5",13,10,\
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur",13,10,\
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC",13,10,\
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc",13,10,\
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq",13,10,\
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA",13,10,\
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d",13,10,\
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=",13,10,\
+"-----END CERTIFICATE-----",13,10,0
+
+isrg_root_x1_pem_len = $ - isrg_root_x1_pem
+
+;------------------------------------------------------------
+
 START:
         mcall   68, 11                  ; init heap so we can allocate memory dynamically
 
@@ -62,8 +102,8 @@ START:
 
 ; Initialize certificates
         DEBUGF  1, "  . Loading the CA root certificate ..."
-        mov     eax, [mbedtls_test_cas_pem_len]
-        cinvoke mbedtls_x509_crt_parse, cacert, [mbedtls_test_cas_pem], [eax]
+        mov     eax, [isrg_root_x1_pem_len]
+        cinvoke mbedtls_x509_crt_parse, cacert, [isrg_root_x1_pem], [eax]
         cmp     eax, 0
         jb      fail.cert
         DEBUGF  1, " ok (%d skipped)\n", eax
@@ -82,7 +122,7 @@ START:
         jnz     fail.config
         DEBUGF  1,  " ok\n"
 
-        cinvoke mbedtls_ssl_conf_authmode, conf, MBEDTLS_SSL_VERIFY_OPTIONAL
+        cinvoke mbedtls_ssl_conf_authmode, conf, MBEDTLS_SSL_VERIFY_REQUIRED
         cinvoke mbedtls_ssl_conf_ca_chain, conf, cacert, 0
         cinvoke mbedtls_ssl_conf_rng, conf, [mbedtls_ctr_drbg_random], ctr_drbg
 ;        cinvoke mbedtls_ssl_conf_dbg, conf, my_debug, stdout
@@ -220,8 +260,6 @@ library lib_mbedtls,               'mbedtls.obj'
 import  lib_mbedtls, \
 mbedtls_init                   ,   'mbedtls_init'                   ,\
 mbedtls_strerror               ,   'mbedtls_strerror'               ,\
-mbedtls_test_cas_pem           ,   'mbedtls_test_cas_pem'           ,\
-mbedtls_test_cas_pem_len       ,   'mbedtls_test_cas_pem_len'       ,\
 mbedtls_x509_crt_free          ,   'mbedtls_x509_crt_free'          ,\
 mbedtls_x509_crt_init          ,   'mbedtls_x509_crt_init'          ,\
 mbedtls_x509_crt_parse         ,   'mbedtls_x509_crt_parse'         ,\
@@ -252,42 +290,4 @@ mbedtls_ssl_get_verify_result  ,   'mbedtls_ssl_get_verify_result'  ,\
 mbedtls_ssl_handshake          ,   'mbedtls_ssl_handshake'          ,\
 mbedtls_ssl_init               ,   'mbedtls_ssl_init'               ,\
 mbedtls_ssl_read               ,   'mbedtls_ssl_read'               ,\
-mbedtls_ssl_set_bio            ,   'mbedtls_ssl_set_bio'            ,\
-mbedtls_ssl_set_hostname       ,   'mbedtls_ssl_set_hostname'       ,\
-mbedtls_ssl_setup              ,   'mbedtls_ssl_setup'              ,\
-mbedtls_ssl_write              ,   'mbedtls_ssl_write'
-
-include_debug_strings
-
-pers db "ssl_client1"
-  .len = $ - pers
-
-exc_sz  db "  ! ", 0
-
-SERVER_PORT db "443", 0
-SERVER_NAME db "kolibrios.org", 0
-GET_REQUEST db "GET / HTTP/1.1", 13, 10
-            db "Host: kolibrios.org", 13, 10, 13, 10
-  .len = $ - GET_REQUEST
-            db 0
-
-IM_END:
-
-; Uninitialize data
-
-;exit_code           dd -1       ;?
-server_fd       mbedtls_net_context
-;flags           dd ?
-rcv_buf         rb BUFFER_LENGTH + 1
-
-entropy         mbedtls_entropy_context
-ctr_drbg        mbedtls_ctr_drbg_context
-ssl             mbedtls_ssl_context
-conf            mbedtls_ssl_config
-cacert          mbedtls_x509_crt
-vrfy_buf        rb 512
-
-I_END:
-
-
-
+mb
